0bab8f5c317b9ce606ab9ddad11e950a21bce0e1
bluefuton <chris@bluefuton.com>
2016-06-27 18:19:42 +0100

    Only show graduation bar if there are recommendations

--- client/reader/start/main.jsx
+++ client/reader/start/main.jsx
@@ -67,14 +67,15 @@ const Start = React.createClass( {
 		const hasRecommendations = this.props.recommendationIds.length > 0;
 		return (
 			<Main className="reader-start">
-
 				{ /* Have not followed a site yet */ }
-				<div className="reader-start__bar is-follow">
-					<span className="reader-start__bar-text">{ this.translate( 'Follow one or more sites to get started' ) }</span>
-				</div>
+				{ ! canGraduate && hasRecommendations &&
+					<div className="reader-start__bar is-follow">
+						<span className="reader-start__bar-text">{ this.translate( 'Follow one or more sites to get started' ) }</span>
+					</div>
+				}
 
 				{ /* Following at least one or more sites */ }
-				{ canGraduate &&
+				{ canGraduate && hasRecommendations &&
 					<div className="reader-start__bar is-following">
 						<span className="reader-start__bar-text">
 							{
